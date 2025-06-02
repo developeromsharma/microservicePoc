f66bc7b12e5a1c8710bf210c517bc3d48b819722
OM SHARMA <developeromsharma@gmail.com>
2025-06-03 01:14:38 +0530

    added auth service

---
+++ AuthService/AuthService.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using AuthService.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthCheckService _authCheckService;
+
+        public AuthController(IAuthCheckService authCheckService)
+        {
+            _authCheckService = authCheckService;
+        }
+
+        [HttpGet("{userId}/{assetId}")]
+        public async Task<IActionResult> CheckEligibility(int userId, int assetId)
+        {
+            var result = await _authCheckService.IsEligibleAsync(userId, assetId);
+            return Ok(result);
+        }
+    }
+
+}
---
+++ AuthService/AuthService.Application/Services/AuthCheckService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthService.Domain.Interfaces;
+using AuthService.Infrastructure.RabbitMQ;
+using System.Net;
+using AuthService.Domain.Entities;
+
+namespace AuthService.Application.Services
+{
+    public class AuthCheckService : IAuthCheckService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IRabbitMqService _rabbitService;
+
+        public AuthCheckService(HttpClient httpClient, IRabbitMqService rabbitService)
+        {
+            _httpClient = httpClient;
+            _rabbitService = rabbitService;
+        }
+
+        public async Task<EligibilityResult> IsEligibleAsync(int userId, int assetId)
+        {
+            var userResponse = await _httpClient.GetAsync($"https://localhost:7247/api/User/{userId}");
+            var assetResponse = await _httpClient.GetAsync($"https://localhost:7060/api/Assets/{assetId}");
+
+            bool isEligible = userResponse.StatusCode == HttpStatusCode.OK && assetResponse.StatusCode == HttpStatusCode.OK;
+
+            var message = new EligibilityResult { IsEligibleForTransaction = isEligible };
+
+            await _rabbitService.SendMessageAsync("dafult", message);
+            await _rabbitService.SendMessageAsync("isEligibleForTransaction", message);
+
+            return message;
+        }
+    }
+
+}
